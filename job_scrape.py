fdfa3de6fde0c62557064a83b059ae7b1e997e46
Sraddheya <ggsraddheya@gmail.com>
2023-11-05 03:56:29 +0000

    Blazing fast 3 job search

--- job_scrape.py
+++ job_scrape.py
@@ -1,3 +1,5 @@
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+anthropic = Anthropic(api_key="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")
 # # Description: This file is used to scrape job postings from Indeed.com using the Claude 2 LLM API.
 # from langchain.chat_models import ChatAnthropic
 # from langchain.chains import create_extraction_chain
@@ -26,20 +28,56 @@
 # def extract(content: str, schema: dict):
 #     return create_extraction_chain(schema=schema, llm=llm).run(content)
 
+output_format = """{
+    "job_title": xxxx,
+    "company": xxxx,
+    "location": xxxx,
+    "description": xxxx,
+    "salary": xxxx,
+    "application_deadline": xxxx,
+    "required_skills": xxxx,
+    "experience_level": xxxx,
+    "education_level": xxxx,
+    "date_posted": xxxx}"""
 
 from bs4 import BeautifulSoup
+from selenium import webdriver
+
 import requests
-
-
-url = 'https://www.indeed.com/jobs?q=data+engineer&l=United+States'
+base_url = 'https://www.reed.co.uk'
+url = 'https://www.reed.co.uk/jobs/software-design-engineer-jobs-in-london'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 response = requests.get(url, headers=headers)
-data = response.text
-soup = BeautifulSoup(data, 'html.parser')
-jobs = soup.find_all('div',{'class':'jobsearch-SerpJobCard'})
+soup = BeautifulSoup(response.content, 'html.parser')
+jobs = soup.find_all('a',{'class':'job-card_jobCard__blockLink__PeeZx'})[:3]
 
-print(data)
+for job_link in jobs:
+    print('----------------')
+    print()
+    url = base_url + job_link.get('href')
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    job = soup.find('div',{'class':'col-lg-12'})
+    job_summary = anthropic.completions.create(
+        model="claude-2",
+        max_tokens_to_sample=3000,
+        prompt=f"{HUMAN_PROMPT} You will be given some html that contains a descriptions for a job. Extract the following information and present it as a where each element is in the JSON format: {output_format}. Provide only the JSON and nothing else. Make sure to remove all html tags and newline characters from the text <html>{job}</html> {AI_PROMPT}",
+    )
+    jjson = job_summary.completion
+    print(jjson)
 
-for job in jobs:
-    print(job)
+
+# # Name
+
+
+
+# # for job in jobs:
+# #    print(job)
+
+# options = webdriver.ChromeOptions()
+# options.add_argument('headless')
+# browser = webdriver.Chrome(options=options)
+# browser.get(url)
+# html = browser.page_source
+# print(html)
