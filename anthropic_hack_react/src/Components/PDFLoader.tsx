c96885814309a3cd365d7b09066571361e3f80b5
rohan-nittur <rohan.nittur@gmail.com>
2023-11-05 04:36:49 +0000

    extracted cv data into JSON

--- anthropic_hack_react/src/Components/PDFLoader.tsx
+++ anthropic_hack_react/src/Components/PDFLoader.tsx
@@ -1,49 +1,136 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+// const PdfUploader: React.FC = () => {
+//   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+//   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const fileList = event.target.files;
+//     if (fileList) {
+//       const file: File = fileList[0];
+//       if (file.type === 'application/pdf') {
+//         setSelectedFile(file);
+//       } else {
+//         alert('Please upload a PDF file.');
+//       }
+//     }
+//   };
+
+//   const handleFileSubmit = async () => {
+//     if (selectedFile) {
+//       try {
+//         const formData = new FormData();
+//         formData.append('pdfFile', selectedFile);
+
+//         const response = await fetch('http://localhost:5000/upload', {
+//           method: 'POST',
+//           body: formData,
+//         });
+
+//         if (response.ok) {
+//           // Handle success
+//           console.log('PDF file uploaded successfully');
+//         } else {
+//           // Handle errors
+//           console.error('Error uploading PDF file');
+//         }
+//       } catch (error) {
+//         // Handle network errors
+//         console.error('Network error:', error);
+//       }
+//     } else {
+//       alert('Please select a file to upload.');
+//     }
+//   };
+
+// //   const handleFileSubmit = async () => {
+// //     if (selectedFile) {
+// //         const formData = new FormData();
+// //         formData.append('file', selectedFile);
+
+// //       try {
+// //         const response = await axios.post('http://localhost:5000/upload', formData, {
+// //           headers: {
+// //             'Content-Type': 'multipart/form-data'
+// //           }
+// //         });
+// //         console.log('File uploaded:', response.data);
+// //       } catch (error) {
+// //         console.error('Error uploading file:', error);
+
+
+
+//     //   const reader = new FileReader();
+//     //   reader.onload = (event) => {
+//     //     // Do whatever you want with the file data (e.g., upload to a server, store in state, etc.)
+//     //     const uploadedFileData = event.target?.result;
+//     //     console.log('Uploaded File Data:', uploadedFileData);
+//     //     // Example: Send the file data to the server
+//     //     // uploadFileToServer(uploadedFileData);
+//     //   };
+//     //   reader.readAsDataURL(selectedFile);
+//     // } else {
+//     //   alert('Please select a file to upload.');
+//     // }
+
+//   return (
+//     <div className="container mt-4">
+//       <div className="row">
+//         <div className="col-6 offset-3">
+//           <input type="file" className="form-control mb-3" accept=".pdf" onChange={handleFileUpload} />
+//           <button className="btn btn-primary" onClick={handleFileSubmit}>
+//             Upload PDF
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 const PdfUploader: React.FC = () => {
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const fileList = event.target.files;
-    if (fileList) {
-      const file: File = fileList[0];
-      if (file.type === 'application/pdf') {
-        setSelectedFile(file);
-      } else {
-        alert('Please upload a PDF file.');
+    const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const fileList = event.target.files;
+      if (fileList) {
+        const file: File = fileList[0];
+        if (file.type === 'application/pdf') {
+          setSelectedFile(file);
+        } else {
+          alert('Please upload a PDF file.');
+        }
       }
-    }
-  };
+    };
 
-  const handleFileSubmit = () => {
-    if (selectedFile) {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        // Do whatever you want with the file data (e.g., upload to a server, store in state, etc.)
-        const uploadedFileData = event.target?.result;
-        console.log('Uploaded File Data:', uploadedFileData);
-        // Example: Send the file data to the server
-        // uploadFileToServer(uploadedFileData);
-      };
-      reader.readAsDataURL(selectedFile);
-    } else {
-      alert('Please select a file to upload.');
-    }
-  };
+    const handleFileSubmit = () => {
+      if (selectedFile) {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          // Do whatever you want with the file data (e.g., upload to a server, store in state, etc.)
+          const uploadedFileData = event.target?.result;
+          console.log('Uploaded File Data:', uploadedFileData);
+          // Example: Send the file data to the server
+          // uploadFileToServer(uploadedFileData);
+        };
+        reader.readAsDataURL(selectedFile);
+      } else {
+        alert('Please select a file to upload.');
+      }
+    };
 
-  return (
-    <div className="container mt-4">
-      <div className="row">
-        <div className="col-6 offset-3">
-          <input type="file" className="form-control mb-3" accept=".pdf" onChange={handleFileUpload} />
-          <button className="btn btn-primary" onClick={handleFileSubmit}>
-            Upload PDF
-          </button>
+    return (
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-6 offset-3">
+            <input type="file" className="form-control mb-3" accept=".pdf" onChange={handleFileUpload} />
+            <button className="btn btn-primary" onClick={handleFileSubmit}>
+              Upload PDF
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default PdfUploader;
--- data_extraction.py
+++ data_extraction.py
@@ -4,45 +4,66 @@
 # pip install anthropic
 
 import PyPDF2
- 
+
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+from flask_cors import CORS
+import os
+
+# app = Flask(__name__)
+# CORS(app)
+
+# @app.route('/uploadpdf', methods=['POST'])
+# def upload_pdf():
+#     if 'pdfFile' in request.files:
+#         pdf_file = request.files['pdfFile']
+#         # Process the PDF file here, e.g., save it, perform operations, etc.
+#         # Example: pdf_file.save('uploaded_file.pdf')
+#         print(pdf_file)
+#         return 'PDF file uploaded successfully'
+#     else:
+#         return 'No PDF file received', 400
+
+# if __name__ == '__main__':
+#     app.run()
+
 #create file object variable
 #opening method will be rb
 pdffileobj=open('test_cv.pdf','rb')
- 
+
 #create reader variable that will read the pdffileobj
 pdfreader=PyPDF2.PdfReader(pdffileobj)
- 
+
 #This will store the number of pages of this pdf file
 x=len(pdfreader.pages)
 print("NUMBER OF PAGES " + str(x))
- 
+
 #create a variable that will select the selected number of pages
 pageobj=pdfreader.pages[x - 1]
- 
+
 #(x+1) because python indentation starts with 0.
 #create text variable which will store all text datafrom pdf file
 text=pageobj.extract_text()
- 
-#save the extracted data from pdf to a txt file
-#we will use file handling here
-#dont forget to put r before you put the file path
-#go to the file location copy the path by right clicking on the file
-#click properties and copy the location path and paste it here.
-#put "\\your_txtfilename"
-#file1=open(r"C:\Users\ggsra\Projects\hack_anthropic\test_cv.txt","a")
-#file1.writelines(text)
 
-#-----------------------------------------------------------------------
 
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 
+output_format = """{
+    "name": xxxx,
+    "experience": xxxx}"""
+
+job_format = """{
+    "job_title": xxxx,
+    "job_duration":xxxx,
+    "company": xxxx,
+    "skills": xxxx}"""
+
 # Name
 anthropic = Anthropic(api_key="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")
-nameCompletion = anthropic.completions.create(
+completion = anthropic.completions.create(
     model="claude-2",
     max_tokens_to_sample=300,
-    prompt=f"{HUMAN_PROMPT} What is the name of the owner of this resume? Return only the name. <resume>{text}<resume> {AI_PROMPT}",
+    prompt=f"{HUMAN_PROMPT} You will be extracting the most useful information from a resume. Extract the following information and present it in the JSON format: {output_format} and repeat this for every job listed in the JSON format {job_format}. Do not provide any preambke or closing, just the raw JSON. Extract the tools listed in each job description by their mentions. Round the job durations to their nearest whole month, for example if someone has been in a role from September 2021 to September 2021 this will count as 1 month and November 2021 to April 2022 will count as 6 months. <resume>{text}<resume> {AI_PROMPT}",
 )
-name = nameCompletion.completion
-print(name)
-
+result = completion.completion
+print(result)
